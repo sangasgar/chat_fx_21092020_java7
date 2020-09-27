16dac613336a644a1b6aebb13fc7eca9ba422fbb
Garegin <sangas@yandex.ru>
2020-09-27 12:36:09 +0300

    Сделано авторзация и регистрация через БД

--- server/src/main/java/server/SimpleAuthService.java
+++ server/src/main/java/server/SimpleAuthService.java
@@ -1,9 +1,16 @@
 package server;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleAuthService implements AuthService {
+    private static Connection connection;
+    private static Statement stmt;
+
+    private static String logPassNick;
+    private static String token[];
+    private static PreparedStatement psInsert;
     private class UserData {
         String login;
         String password;
@@ -19,14 +26,20 @@ public class SimpleAuthService implements AuthService {
     List<UserData> users;
 
     public SimpleAuthService() {
-        users = new ArrayList<>();
-        for (int i = 1; i <= 10; i++) {
-            users.add(new UserData("login" + i, "pass" + i, "nick" + i));
+        try {
+            connectDb ();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
-
+        users = new ArrayList<>();
+        token = logPassNick.split("///");
         users.add(new UserData("qwe" , "qwe" , "qwe" ));
         users.add(new UserData("asd" , "asd" , "asd" ));
         users.add(new UserData("zxc" , "zxc" , "zxc" ));
+        users.add(new UserData(token[0] , token[1] , token[2] ));
+
     }
 
     @Override
@@ -41,12 +54,40 @@ public class SimpleAuthService implements AuthService {
 
     @Override
     public boolean registration(String login, String password, String nickname) {
+
         for (UserData user : users) {
             if (user.login.equals(login) || user.nickname.equals(nickname)) {
                 return false;
             }
         }
         users.add(new UserData(login , password, nickname ));
+        try {
+
+            System.out.println(login + nickname + password);
+            psInsert = connection.prepareStatement("INSERT INTO users (name, nick, password) VALUES (?, ?, ?);");
+            connection.setAutoCommit(false);
+                psInsert.setString(1, login);
+                psInsert.setString(3, password);
+                psInsert.setString(2, nickname);
+                psInsert.executeUpdate();
+            connection.setAutoCommit(true);
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return true;
+    }
+    public static void connectDb () throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:baseChat.db");
+        stmt = connection.createStatement();
+        System.out.println("connect ok");
+        ResultSet rs = stmt.executeQuery("SELECT name, password, nick FROM users WHERE name = \"garik\" ");
+        //      stmt.executeUpdate("INSERT INTO students (name,score) VALUES (\"Garik\", 80),(\"Aleksei\", 50);");
+        while (rs.next()) {
+            logPassNick =  rs.getString("name") + "///" + rs.getString("password")  + "///" + rs.getString("nick");
+
+        }
     }
 }
